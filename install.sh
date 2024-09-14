e3659a2caf73546fca03b51923b8e8dad5aa9506
Abdul Rehman <ar618211@gmail.com>
2024-09-14 20:30:34 +0500

    dracula in nvim, kitty and picom in i3

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
+ln -s $HOME/dotfiles/tmux $HOME/.config/tmux
+ln -s $HOME/dotfiles/i3 $HOME/.config/i3
+ln -s $HOME/dotfiles/i3status $HOME/.config/i3status
+ln -s $HOME/dotfiles/picom $HOME/.config/picom
+ln -s $HOME/dotfiles/kitty $HOME/.config/kitty
+
