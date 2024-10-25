89b5033793d963795e21a78eacbc6dc8c31f9e6d
Abdul Rehman <ar618211@gmail.com>
2024-10-25 14:22:42 +0500

    added default qtile config

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
 ln -s $HOME/dotfiles/tmux $HOME/.config/tmux
+ln -s $HOME/dotfiles/qtile $HOME/.config/qtile
 ln -s $HOME/dotfiles/i3 $HOME/.config/i3
 ln -s $HOME/dotfiles/i3status $HOME/.config/i3status
 ln -s $HOME/dotfiles/picom $HOME/.config/picom
