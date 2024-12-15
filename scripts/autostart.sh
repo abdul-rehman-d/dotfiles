c07cfaaae6fdc71f048476a2c455798456aa6b08
Abdul Rehman <ar618211@gmail.com>
2024-12-15 19:28:08 +0500

    added wallpapers and using feh and setting rand wallpaper

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+get_rand_wallpaper() {
+  num=$(ls -1 $HOME/dotfiles/wallpapers | wc -l)
+  rand=$((1 + $RANDOM % $num))
+  ls -1 $HOME/dotfiles/wallpapers | sed -n "${rand}p"
+}
+
+
 picom -c &
-nitrogen --restore &
+# nitrogen --restore &
+feh --bg-fill $HOME/dotfiles/wallpapers/$(get_rand_wallpaper)
 nm-applet &
 blueman-applet &
 volumeicon &
