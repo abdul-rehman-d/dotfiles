d4a7e3787a4c7768fa06f7c32251a4a46b5d510e
Abdul Rehman <ar618211@gmail.com>
2025-03-09 22:48:57 +0500

    added harpooooon

--- nvim/init.lua
+++ nvim/init.lua
@@ -676,6 +676,35 @@ require("lazy").setup({
 	},
 
 	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local harpoon = require("harpoon")
+			harpoon:setup() -- Initialize Harpoon
+
+			-- Define keymaps after setup
+			vim.keymap.set("n", "<leader>ha", function()
+				harpoon:list():append()
+			end, { desc = "Harpoon Add File" })
+			vim.keymap.set("n", "<leader>hm", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end, { desc = "Harpoon Menu" })
+			vim.keymap.set("n", "<leader>h1", function()
+				harpoon:list():select(1)
+			end, { desc = "Harpoon to File 1" })
+			vim.keymap.set("n", "<leader>h2", function()
+				harpoon:list():select(2)
+			end, { desc = "Harpoon to File 2" })
+			vim.keymap.set("n", "<leader>h3", function()
+				harpoon:list():select(3)
+			end, { desc = "Harpoon to File 3" })
+			vim.keymap.set("n", "<leader>h4", function()
+				harpoon:list():select(4)
+			end, { desc = "Harpoon to File 4" })
+		end,
+	},
+	{
 		"Mofiqul/dracula.nvim",
 		priority = 1000,
 		opts = {
