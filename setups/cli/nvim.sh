b66ba226d05138eb18eb819db74e765cff4671fa
Abdul Rehman <ar618211@gmail.com>
2025-04-03 19:26:44 +0500

    fix: nvim setup script trial 4

--- setups/cli/nvim.sh
+++ setups/cli/nvim.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-sudo apt install fzf wget -y
+sudo apt install fzf -y
 
 # Install neovim
-wget https://github.com/neovim/neovim/releases/download/v0.11.0/nvim-linux-x86_64.tar.gz
-tar xzvf nvim-linux-x86-64.tar.gz -C $HOME/
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+tar -xzf nvim-linux-x86-64.tar.gz -C $HOME/
 mv $HOME/nvim-linux-x86_64 $HOME/.nvim
+rm nvim-linux-x86_64.tar.gz
 
 ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
