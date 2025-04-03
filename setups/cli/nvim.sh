3703681836bf593e746e7ee697f3201da507f8ac
Abdul Rehman <ar618211@gmail.com>
2025-04-03 19:33:40 +0500

    fix: nvim setup finally fixed

--- setups/cli/nvim.sh
+++ setups/cli/nvim.sh
@@ -4,7 +4,7 @@ sudo apt install fzf -y
 
 # Install neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-tar -xzf nvim-linux-x86-64.tar.gz -C $HOME/
+tar -xzf nvim-linux-x86_64.tar.gz -C $HOME/
 mv $HOME/nvim-linux-x86_64 $HOME/.nvim
 rm nvim-linux-x86_64.tar.gz
 
