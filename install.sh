ac70794cc0878a964006db6beb09b21db80831db
Abdul Rehman <ar618211@gmail.com>
2025-04-03 19:38:58 +0500

    testing why flag not working 2

--- install.sh
+++ install.sh
@@ -6,20 +6,29 @@ echo "## Dotfiles Installer ##"
 echo "#########################"
 
 echo "All arguments: $@"
+if [[ "$1" == "--cli-only" ]]; then
+	echo "Running in CLI only mode. Skipping GUI setup."
+else
+	echo "Running in full mode. Setting up GUI as well."
+fi
 exit 0
 
+echo ""
 echo "################### UPDATE SYSTEM ###################"
 sudo apt update
 sudo apt upgrade -y
 
+echo ""
 echo "################### INSTALLING DEPENDENCIES ###################"
 sudo apt install git -y
 
+echo ""
 echo "################### CLONING DOTFILES ###################"
 git clone https://github.com/abdul-rehman-d/dotfiles.git ~/dotfiles
 
 mkdir -p ~/.config
 
+echo ""
 echo "################### SETTING UP CLI SOFTWARE ###################"
 # iterate over ~/dotfiles/setups/cli/
 for file in ~/dotfiles/setups/cli/*; do
