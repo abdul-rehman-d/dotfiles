813a2ae04e6ccd4e24d0872e1c98f6ba3040b9a1
Abdul Rehman <ar618211@gmail.com>
2025-04-04 00:46:31 +0500

    fix: qtile installation

--- setups/gui/qtile.sh
+++ setups/gui/qtile.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo apt install xserver-xorg xinit
-sudo apt install libpangocairo-1.0-0
-sudo apt install python3-pip python3-xcffib python3-cairocffi
+sudo apt install -y xserver-xorg xinit
+sudo apt install -y libpangocairo-1.0-0
+sudo apt install -y python3-pip python3-xcffib python3-cairocffi
 
-sudo apt install flameshot feh copyq -y
+sudo apt install -y flameshot feh copyq
 
-pip install qtile
+sudo pip install qtile --break-system-packages
 
 ln -s $HOME/dotfiles/qtile $HOME/.config/qtile
 sudo cp $HOME/dotfiles/qtile/qtile.desktop /usr/share/xsessions/
