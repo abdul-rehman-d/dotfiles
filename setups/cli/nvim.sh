ca894c123d53d87c16ba271bdfb8fe05797157ff
Abdul Rehman <ar618211@gmail.com>
2025-04-03 19:12:46 +0500

    fix: nameing issue in nvim script

--- setups/cli/nvim.sh
+++ setups/cli/nvim.sh
@@ -4,7 +4,7 @@ sudo apt install fzf -y
 
 # Install neovim
 wget https://github.com/neovim/neovim/releases/download/v0.11.0/nvim-linux-x86_64.tar.gz
-tar xzvf nvim-linux-arm64.tar.gz -C $HOME/
+tar xzvf nvim-linux-x86-64.tar.gz -C $HOME/
 mv $HOME/nvim-linux-x86_64 $HOME/.nvim
 
 ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
