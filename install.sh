6ee4d6c4d31764b8ae43dbfc20d2a07da449ebaa
github-name-here <github-email-here>
2025-04-04 15:09:24 +0500

    git work probably done

--- install.sh
+++ install.sh
@@ -20,6 +20,23 @@ echo -e "\e[44m################### CLONING DOTFILES ###################\e[0m"
 git clone https://github.com/abdul-rehman-d/dotfiles.git ~/dotfiles
 
 mkdir -p ~/.config
+mkdir -p ~/.local/bin
+mkdir -p ~/dev
+mkdir -p ~/dev/personal/
+mkdir -p ~/dev/work/
+
+echo "If you want to setup ssh keys for github, please enter personal and/or work email addresses."
+read -p "Personal Github email: " personal_email
+read -p "Personal Github ssh passcode: " personal_passcode
+read -p "Work email: " work_email
+read -p "Work Github ssh passcode: " work_passcode
+read -p "Name: " name
+
+export PERSONAL_EMAIL=$personal_email
+export PERSONAL_PASSPHRASE=$personal_passcode
+export WORK_EMAIL=$work_email
+export WORK_PASSPHRASE=$work_passcode
+export NAME=$name
 
 echo ""
 echo -e "\e[44m################### SETTING UP CLI SOFTWARE ###################\e[0m"
@@ -50,3 +67,9 @@ for file in ~/dotfiles/setups/gui/*; do
 	    fi
 	fi
 done
+
+echo ""
+echo -e "\e[44m################### DONE ###################\e[0m"
+echo "Next Step: "
+echo "1. Add SSH keys to Github (~/.ssh/git-personal.pub and ~/ssh/git-work.pub)"
+echo "2. Reboot the system and select the Qtile session from the login screen."
--- setups/cli/git.sh
+++ setups/cli/git.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-echo "Enter your personal email:"
-read personal_email
+echo $PERSONAL_EMAIL
+echo $PERSONAL_PASSPHRASE
+echo $WORK_EMAIL
+echo $WORK_PASSPHRASE
+echo $NAME
 
-echo "Enter your work email:"
-read work_email
+if [[ -n $PERSONAL_EMAIL ]]; then
+	ssh-keygen -t rsa -b 4096 -C "$PERSONAL_EMAIL" -f "$HOME/.ssh/git-personal" -N "$PERSONAL_PASSPHRASE"
 
-ssh-keygen -t rsa -b 4096 -C "$personal_email" -f "$HOME/.ssh/git-personal" -N ""
+	sed -i "s/<github-email-here>/${PERSONAL_EMAIL}/g" $HOME/dotfiles/gitconfig
+	sed -i "s/<github-name-here>/${NAME}/g" $HOME/dotfiles/gitconfig
 
-ssh-keygen -t rsa -b 4096 -C "$work_email" -f "$HOME/.ssh/git-work" -N ""
+	ln -s $HOME/dotfiles/gitconfig $HOME/.gitconfig
+fi
 
-# symlinks
-ln -s $HOME/dotfiles/gitconfig $HOME/.gitconfig
-ln -s $HOME/dotfiles/gitconfig-work $HOME/.gitconfig-work
+if [[ -n $WORK_EMAIL ]]; then
+	ssh-keygen -t rsa -b 4096 -C ${WORK_EMAIL} -f "$HOME/.ssh/git-work" -N "$WORK_PASSPHRASE"
+
+	sed -i "s/<github-work-email-here>/${WORK_EMAIL}/g" $HOME/dotfiles/gitconfig-work
+	sed -i "s/<github-name-here>/${NAME}/g" $HOME/dotfiles/gitconfig-work
+
+	ln -s $HOME/dotfiles/gitconfig-work $HOME/.gitconfig-work
+fi
 
