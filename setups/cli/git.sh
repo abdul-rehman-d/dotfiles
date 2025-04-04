06791bd0988c143695cdba4e4274cf0b8c78b6d0
github-name-here <github-email-here>
2025-04-04 15:18:57 +0500

    gitconfig copying instead of symlinks

--- setups/cli/git.sh
+++ setups/cli/git.sh
@@ -9,18 +9,18 @@ echo $NAME
 if [[ -n $PERSONAL_EMAIL ]]; then
 	ssh-keygen -t rsa -b 4096 -C "$PERSONAL_EMAIL" -f "$HOME/.ssh/git-personal" -N "$PERSONAL_PASSPHRASE"
 
-	sed -i "s/<github-email-here>/${PERSONAL_EMAIL}/g" $HOME/dotfiles/gitconfig
-	sed -i "s/<github-name-here>/${NAME}/g" $HOME/dotfiles/gitconfig
+	cp $HOME/dotfiles/gitconfig $HOME/.gitconfig
 
-	ln -s $HOME/dotfiles/gitconfig $HOME/.gitconfig
+	sed -i "s/<github-email-here>/${PERSONAL_EMAIL}/g" $HOME/.gitconfig
+	sed -i "s/<github-name-here>/${NAME}/g" $HOME/.gitconfig
 fi
 
 if [[ -n $WORK_EMAIL ]]; then
 	ssh-keygen -t rsa -b 4096 -C ${WORK_EMAIL} -f "$HOME/.ssh/git-work" -N "$WORK_PASSPHRASE"
 
-	sed -i "s/<github-work-email-here>/${WORK_EMAIL}/g" $HOME/dotfiles/gitconfig-work
-	sed -i "s/<github-name-here>/${NAME}/g" $HOME/dotfiles/gitconfig-work
+	cp $HOME/dotfiles/gitconfig-work $HOME/.gitconfig-work
 
-	ln -s $HOME/dotfiles/gitconfig-work $HOME/.gitconfig-work
+	sed -i "s/<github-work-email-here>/${WORK_EMAIL}/g" $HOME/.gitconfig-work
+	sed -i "s/<github-name-here>/${NAME}/g" $HOME/.gitconfig-work
 fi
 
