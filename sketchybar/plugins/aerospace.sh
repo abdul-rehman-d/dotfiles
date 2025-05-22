1b5cbe24c66c373be929e241a2e9cbb069ccc66d
Abdul Rehman <ar618211@gmail.com>
2025-05-23 00:53:29 +0500

    f events lets just call it constantly

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
+is_active=false
+is_empty=true
+
+FOCUSED_WORKSPACE=$(aerospace list-workspaces --focused)
+
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on
-else
-    sketchybar --set $NAME background.drawing=off
+    is_active=true
 fi
+
+if [ `aerospace list-windows --workspace $1 | wc -l` != "0" ]; then
+    is_empty=false
+fi
+
+sketchybar --set $NAME \
+    background.drawing=$([[ "$is_active" == true ]] && echo "on" || echo "off") \
+    icon.color=$([[ "$is_empty" == true ]] && echo "0x40f8fbf2" || echo "0xfff8fbf2")
+
