fda20eb22130e4ca47062f9f2f03a4753b9fb99c
Abdul Rehman <ar618211@gmail.com>
2026-04-10 16:37:39 +0500

    chore: nvim gdscript

--- nvim/init.lua
+++ nvim/init.lua
@@ -458,6 +458,9 @@ require("lazy").setup({
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
+			-- GDscript
+			require("lspconfig").gdscript.setup(capabilities)
+
 			-- Enable the following language servers
 			--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
 			--
