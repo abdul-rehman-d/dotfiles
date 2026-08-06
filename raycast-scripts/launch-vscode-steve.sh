322d0182493186263790cea44e10d9130dd3969f
Abdul Rehman <ar618211@gmail.com>
2026-08-07 03:41:01 +0500

    feat: workspace arg on raycast script to open portable instance

--- raycast-scripts/launch-vscode-steve.sh
+++ raycast-scripts/launch-vscode-steve.sh
@@ -8,7 +8,18 @@
 # Optional parameters:
 # @raycast.icon 💼
 # @raycast.packageName VS Code
+# @raycast.argument1 { "type": "text", "placeholder": "location", "optional": true }
 
-open -n "/Applications/Visual Studio Code.app" --args \
-  --user-data-dir="$HOME/vscode-steve/user-data" \
+location="$1"
+
+args=(
+  --user-data-dir="$HOME/vscode-steve/user-data"
   --extensions-dir="$HOME/vscode-steve/extensions"
+  --remote tunnel+wf19806
+)
+
+if [[ -n "$location" ]]; then
+  args+=("$location")
+fi
+
+open -n "/Applications/Visual Studio Code.app" --args "${args[@]}"
